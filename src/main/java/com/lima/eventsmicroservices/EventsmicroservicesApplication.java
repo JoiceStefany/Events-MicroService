ad9b232b5ae17dc3b6dad2a94adb99a2e85a3516
JoiceStefany <steffanyjoice006@gmail.com>
2023-11-30 15:24:32 -0300

    Initial Project

---
+++ src/main/java/com/lima/eventsmicroservices/EventsmicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.lima.eventsmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsmicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsmicroservicesApplication.class, args);
+	}
+
+}
