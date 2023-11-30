1d9abeeff475377d6eac922b086e15b78442ca14
JoiceStefany <steffanyjoice006@gmail.com>
2023-11-30 15:41:29 -0300

    Events

---
+++ src/main/java/com/lima/eventsmicroservices/domain/Event.java
@@ -0,0 +1,32 @@
+package com.lima.eventsmicroservices.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "event")
+@Table(name = "event")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private int maxParticipants;
+    private int registeredParticipants;
+    private String date;
+    private String title;
+    private String description;
+    
+}
