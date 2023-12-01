9700253bf8f8a6292c91593eb103b247194d7a1f
JoiceStefany <steffanyjoice006@gmail.com>
2023-12-01 12:58:54 -0300

    Event Repository

---
+++ src/main/java/com/lima/eventsmicroservices/repository/EventRepository.java
@@ -0,0 +1,23 @@
+package com.lima.eventsmicroservices.repository;
+
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.lima.eventsmicroservices.domain.Event;
+
+import jakarta.annotation.Nonnull;
+
+public interface EventRepository extends JpaRepository <Event, String> {
+    
+   @Query(value = "SELECT * FROM events e WHERE PARSE(e.date AS TIMESTAMP) > :currentDate", nativeQuery = true)
+   List<Event> findUpcomingEvents(@Param("currentDate") LocalDateTime currentDate);
+
+   @Nonnull
+   Optional<Event>  findById(@Nonnull String id);
+}
