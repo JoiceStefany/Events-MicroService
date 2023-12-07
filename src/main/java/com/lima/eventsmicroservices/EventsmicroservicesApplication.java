d75ecf3d14a10e34d148812cd1404041eea0d3f9
JoiceStefany <steffanyjoice006@gmail.com>
2023-12-07 15:10:43 -0300

    Annotation Enable Feign Client

--- src/main/java/com/lima/eventsmicroservices/EventsmicroservicesApplication.java
+++ src/main/java/com/lima/eventsmicroservices/EventsmicroservicesApplication.java
@@ -2,8 +2,10 @@ package com.lima.eventsmicroservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class EventsmicroservicesApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/lima/eventsmicroservices/services/EmailServiceClient.java
+++ src/main/java/com/lima/eventsmicroservices/services/EmailServiceClient.java
@@ -4,10 +4,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.lima.eventsmicroservices.dtos.EmailRequestDTO;
+
 @FeignClient(name = "email-service", url = "http://localhost:8090/api/email");
 public interface EmailServiceClient {
 
     @PostMapping("/send")
-    void sendEmail(@RequestBody EmailRequest EmailRequest);
+    void sendEmail(@RequestBody EmailRequestDTO emailRequest);
     
 }
